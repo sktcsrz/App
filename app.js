533945465c74fae430b2bb16e614c0bce5e068b3
srz <sb5846868@gmail.com>
2026-03-04 09:21:10 +0545

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const firebaseConfig = {
 const IMGBB_API_KEY = "83cdd14bdbb113e25ea67c22b5864208"; 
 
 // THE GOOGLE EMAIL THAT HAS ADMIN ACCESS
-const ADMIN_EMAIL = "sb584688@gmail.com"; 
+const ADMIN_EMAIL = "sb5846868@gmail.com"; 
 
 // ==========================================
 // 2. INITIALIZATION
@@ -662,4 +662,5 @@ if (document.getElementById('admin-app'))
         btn.textContent = "Add Method";
 
     };
+
 
